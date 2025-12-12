652d9e3237f6dc2087571b6e2f83a8ef226af9fb
Subham0813 <subham913dces3@gmail.com>
2025-12-12 16:13:49 +0530

    fix:log added for file upload

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import upload from "../utils/uploadFileUsingMulter.js";
+import upload from "../middlewares/upload.js";
 
 import {
   handleGetFiles,
@@ -8,13 +8,31 @@ import {
   handleMoveToBinFile,
   handleRestoreFile,
 } from "../controllers/apiControllers.js";
-import { validateParent } from "../middlewares/validateParentId.js";
+import { validateParent } from "../middlewares/validate.js";
+import { MulterError } from "multer";
 
 const router = Router();
 
+const uploadFile = (req, res, next) => {
+  upload.single("file")(req, res, (err) => {
+    if (err instanceof MulterError) {
+      // A Multer error occurred when uploading.
+      console.error("Multer error", err.name, err.message);
+      return next("Multer Error occurred!!");
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      console.error("Other error", err.name, err.message);
+      return next("Unknown error occured!!");
+    }
+    // Everything went fine.
+    console.error("Upload successfull..✨");
+    return next();
+  });
+};
+
 //Create
-router.post("/", upload.single("file"), handleCreateFile);
-router.post("/:dirId", validateParent, upload.single("file"), handleCreateFile);
+router.post("/", uploadFile, handleCreateFile);
+router.post("/:dirId", validateParent, uploadFile, handleCreateFile);
 
 //Read
 router.get("/:id", handleGetFiles);
@@ -25,5 +43,7 @@ router.patch("/:id", handleUpdateFile);
 //delete
 router.post("/:id/trash", handleMoveToBinFile);
 router.post("/:id/restore", handleRestoreFile);
+
+// router.delete('/:id', handleDelete)
 
 export default router;
