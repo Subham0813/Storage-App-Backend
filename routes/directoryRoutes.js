ffcfe270760ca984f97d56f797c0e50a609f6618
Subham0813 <subham913dces3@gmail.com>
2025-12-16 02:43:57 +0530

    feat:new Routes added -FileControllers: -GET metadata , preview and download -DELETE (file) -DirectoryControllers: -GET download -DELETE (folder)

--- routes/directoryRoutes.js
+++ routes/directoryRoutes.js
@@ -2,18 +2,21 @@ import { Router } from "express";
 
 import {
   handleGetDirectories,
+  handleDownloadDirectory,
   handleCreateDirectory,
   handleUpdateDirectory,
   handleMoveToBinDirectory,
   handleRestoreDirectory,
+  handleDeleteDirectory
   // handleDelete
-} from "../controllers/apiControllers.js";
+} from "../controllers/DirectoryControllers.js";
 import { validateParent } from "../middlewares/validate.js";
 
 const router = Router();
 
 //read
 router.get("/:id", handleGetDirectories);
+router.get("/:id/download", handleDownloadDirectory);
 
 //create
 router.post("/", handleCreateDirectory);
@@ -26,6 +29,6 @@ router.patch("/:id", handleUpdateDirectory);
 router.post("/:id/trash", handleMoveToBinDirectory);
 router.post("/:id/restore", handleRestoreDirectory);
 
-// router.delete('/:id', handleDelete)
+router.delete('/:id/delete', handleDeleteDirectory)
 
 export default router;
