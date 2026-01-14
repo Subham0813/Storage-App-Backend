bed6d776296c97246bab4af08649cf5ab9cb15ce
Subham0813 <subham913dces3@gmail.com>
2026-01-14 13:44:31 +0530

    fix:Ensuring clean code architecture, codes are moved to separate files, strongly following MVC principles.

--- app.js
+++ app.js
@@ -6,83 +6,50 @@ import authRoutes from "./routes/authRoutes.js";
 import directoryRoutes from "./routes/directoryRoutes.js";
 import fileRoutes from "./routes/fileRoutes.js";
 import homeRoutes from "./routes/homeRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
-import { validateToken } from "./middlewares/validate.js";
-import { connectDb } from "./configs/db.js";
+import { validateSession } from "./middlewares/validateSession.js";
 
-import { Db } from "mongodb";
-import { appendFile } from "fs/promises";
+import connectMongoose from "./configs/connect.js";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
-import "./configs/connect.js";
+const COOKIE_SECRET = process.env.COOKIE_SECRET || "myScrete_Code@138";
 
 try {
-  const db = await connectDb();
+  await connectMongoose();
+
   const app = express();
   const port = 4000;
 
   app.use(serveFavicon(import.meta.dirname + "/public/favicon.ico"));
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
+  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+  app.use(cookieParser(COOKIE_SECRET)); // <= secrete to signed cookie
+
+  app.use("/auth", express.json(), authRoutes);
+
+  app.use("/files", validateSession, uploadRoutes);
+
   app.use(express.json());
 
-  app.use(async (req, res, next) => {
-    req.db = db;
-    next();
-  });
+  app.use("/", validateSession, homeRoutes);
 
-  //cookieParser
-  app.use((req, res, next) => {
-    const cookies = {};
-    if (req.headers && req.headers.cookie) {
-      req.headers.cookie.split("; ").forEach((item) => {
-        const [key, val] = item.split("=");
-        cookies[key] = val;
-      });
-    }
+  app.use("/files", validateSession, fileRoutes);
 
-    req.cookies = cookies;
-    next();
-  });
-
-  app.use("/auth", authRoutes);
-
-  app.use("/", validateToken, homeRoutes);
-
-  app.use("/files", validateToken, fileRoutes);
-
-  app.use("/dirs", validateToken, directoryRoutes);
+  app.use("/dirs", validateSession, directoryRoutes);
 
   // 404 handler
   app.use((req, res) => {
-    return res.status(404).send();
+    return res.status(404).send({success: false, message: "Route not found."});
   });
 
-  app.use(async (err, req, res, next) => {
-    // console.log(err);
-    // console.error(
-    //   err?.errInfo?.details?.schemaRulesNotSatisfied[0]
-    //     .propertiesNotSatisfied[0].details
-    // );
-    // await appendFile("./error.log", JSON.stringify(err.errorResponse));
-    const status = err.statusCode || 500;
-    const message =
-      err?.message?.split(",")[0]?.split(`${err.code}:`)[1]?.trim() ||
-      "Internal server error. Please try again later.";
-
-    return res.status(status).json({
-      success: false,
-      message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(port, () => {
     console.log("server started at port :", port);
   });
-} catch (error) {
-  console.log({ [error.name]: error.message });
+} catch (err) {
+  console.log({ [err.name]: err.message });
   process.exit(1);
 }
